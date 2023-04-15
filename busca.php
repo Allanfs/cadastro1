3a1fe74ce55f8ea5814e648799b495065fa0c059
Givaldo Fernandes <122561742+Gilferprog@users.noreply.github.com>
2023-04-15 08:44:05 -0300

    Criei a busca

---
+++ busca.php
@@ -0,0 +1,10 @@
+<?php include_once"config.php";?>
+<?php 
+$busca = $_GET['busca'];
+$result_nomes =  "SELECT * FROM tbclientes WHERE nome like '%$busca%'";
+$resultado = mysqli_query($conn, $result_nomes);
+while ($linha=mysqli_fetch_array($resultado)){
+	echo $nome = $linha['nome'];
+	echo '<br>';
+}
+?>
